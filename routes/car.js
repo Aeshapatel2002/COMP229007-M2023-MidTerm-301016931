d44e827fe33abfd069a17ba51974c8fec8a546f3
Aeshapatel2002 <apate931@my.centennialcollege.ca>
2023-06-24 10:04:12 -0400

    Updated code

--- routes/car.js
+++ routes/car.js
@@ -4,12 +4,15 @@ var router = express.Router();
 let carController = require('../controllers/car');
 
 // Helper function for guard purposes
-function requireAuth(req, res, next)
-{
-    // check if the user is logged in
-    
-    // ADD YOUR CODE HERE        
-
+function requireAuth(req, res, next) {
+  // Check if the user is logged in
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    // Store the original URL in the session to redirect the user back after successful authentication
+    req.session.originalUrl = req.originalUrl;
+    res.redirect('/users/sign'); // Redirect to the login page if the user is not authenticated
+  }
 }
 
 /* GET list of items */
@@ -19,16 +22,16 @@ router.get('/list', carController.carList);
 router.get('/details/:id', carController.details);
 
 // Routers for edit
-router.get('/edit/:id', carController.displayEditPage);
-router.post('/edit/:id', carController.processEditPage);
+router.get('/edit/:id', requireAuth, carController.displayEditPage);
+router.post('/edit/:id', requireAuth, carController.processEditPage);
 
 // Delete
-router.get('/delete/:id', carController.performDelete);
+router.get('/delete/:id', requireAuth, carController.performDelete);
 
 /* GET Route for displaying the Add page - CREATE Operation */
-router.get('/add', carController.displayAddPage);
+router.get('/add', requireAuth, carController.displayAddPage);
 
 /* POST Route for processing the Add page - CREATE Operation */
-router.post('/add', carController.processAddPage);
+router.post('/add', requireAuth, carController.processAddPage);
 
 module.exports = router;
